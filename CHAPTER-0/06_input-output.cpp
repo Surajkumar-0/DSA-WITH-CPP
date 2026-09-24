ea721ad75afc3ef985fd04c89871e7d37c70800f
Suraj Kumar <surajkumar@Surajs-MacBook-Pro.local>
2026-09-24 11:25:52 +0530

    Added input output

---
+++ CHAPTER-0/06_input-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+
+    cout << "Naam daalo: ";
+    cin >> name;
+
+    cout << "Age daalo: ";
+    cin >> age;
+
+    cout << "\nTera naam: " << name << endl;
+    cout << "Teri age:  " << age << endl;
+
+    return 0;
+}
