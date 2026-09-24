110328854a7eaa7552f1e74ef6e93fe16cc7b8c2
Suraj Kumar <surajkushwaha2659@gmail.com>
2026-09-24 10:14:39 +0530

    Add C++ data types with examples

---
+++ CHAPTER-0/03_data-types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int age = 20;              // Pura number (bina decimal)
+    float price = 10.5f;       // Decimal number (chota)
+    double pi = 3.14159265;    // Decimal number (bada/accurate)
+    char grade = 'A';          // Sirf EK letter/character
+    bool isPass = true;        // Sirf do values: true ya false
+    string name = "Rahul";     // Poora word ya sentence
+
+    cout << age << endl;
+    cout << price << endl;
+    cout << pi << endl;
+    cout << grade << endl;
+    cout << isPass << endl;
+    cout << name << endl;
+
+    return 0;
+}
